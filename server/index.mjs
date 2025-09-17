fb2c9892378b71a08a7deb58c0b56d702a7a5836
iyi7028 <iyi7028@gmail.com>
2025-09-17 09:01:25 +0900

    esp32, clients 서버 listen 추가

--- server/index.mjs
+++ server/index.mjs
@@ -1,4 +1,4 @@
-import net from "net"
+import net from "net";
 
 const ESP32_TCP_SOCKET_PORT = 10032;
 const TCP_SOCKET_PORT = 10000;
@@ -6,18 +6,37 @@ const WEB_SOCKET_PORT = 11000;
 
 let esp32Socket = null;
 let tcpSocketClients = [];
+const esp32TcpSocketServer = net.createServer((socket) => {
+  console.log("TCP Socket client connected");
 
+  //접속한 클라이언트 소켓을 배열에 추가
+  esp32Socket = socket;
 
-const tcpSocketServer = net.createServer((socket) => {
-    console.log("TCP Socket client connected");
+  //소켓이 작동중일때 메시지를 받은 경우
+  socket.on("data", (data) => {
+    console.log("Received from ESP32 TCP socket client : ", data.toString());
 
-    //접속한 클라이언트 소켓을 배열에 추가
-    tcpSocketClients.push(socket);
+    //로그 저장기능
+    // const dataObj = JSON.parse(data.toString);
 
-    //소켓이 작동중일때 메시지를 받은 경우
-    socket.on("data",(data)=>{
-        console.log("Received from TCP socket client : ", data.toString());
-        
-
+    //브로드 캐스트
+    tcpSocketClients.forEach((tcpSocketClient) => {
+      tcpSocketClient.write(data);
     });
+  });
 });
+const tcpSocketServer = net.createServer((socket) => {
+  console.log("TCP Socket client connected");
+
+  //접속한 클라이언트 소켓을 배열에 추가
+  tcpSocketClients.push(socket);
+
+  //소켓이 작동중일때 메시지를 받은 경우
+  socket.on("data", (data) => {
+    console.log("Received from TCP socket client : ", data.toString());
+
+  });
+});
+
+esp32TcpSocketServer.listen(ESP32_TCP_SOCKET_PORT,()=>console.log(`esp32 server is waiting for client connecting : ${ESP32_TCP_SOCKET_PORT}`));
+tcpSocketServer.listen(TCP_SOCKET_PORT,()=>console.log(`server is waiting for client connecting : ${TCP_SOCKET_PORT}`));
