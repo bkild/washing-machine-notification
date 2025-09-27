17fa9af5b7e61d35abc0921ff28405feff99bf4b
iyi7028 <iyi7028@gmail.com>
2025-09-27 23:46:10 +0900

    빛의세기비교를 통한 led on/off 상태체크 구현시작

--- esp32-client/seven_segment_decode_test/seven_segment_decode_test.ino
+++ esp32-client/seven_segment_decode_test/seven_segment_decode_test.ino
@@ -5,11 +5,61 @@
 #include "seven_segment_led.h"
 
 // JPEG 메모리 버퍼에서 디코딩 (전체 변환 아님)
+int off_led_power = 0;
+int segment_led_power[4][8];
 bool segment_led_state[4][8];
+int len_led_area = 3;
+int ret_array[4];
+bool calc_cross_area(int x, int y, int w, int h, int x2, int y2, int w2, int h2) {
+  if (x2 == -1 || y2 == -1) {
+    return false;
+  }
+  if (x2 + w2 - 1 < x || x + w - 1 < x2 || y2 + h2 - 1 < y || y + w - 1 < y2) {
+    return false;
+  }
+  ret_array[0] = x;
+  ret_array[1] = y;
+  ret_array[2] = x + w - 1;
+  ret_array[3] = y + h - 1;
+  if (x < x2) {
+    ret_array[0] = x2;
+  }
+  if (x2 + w2 - 1 < x + w - 1) {
+    ret_array[2] = x2 + w2 - 1;
+  }
+  if (y < y2) {
+    ret_array[1] = y2;
+  }
+  if (y2 + h2 - 1 < y + h - 1) {
+    ret_array[3] = y2 + h2 - 1;
+  }
+  ret_array[0] -= x;
+  ret_array[1] -= y;
+  ret_array[2] -= x;
+  ret_array[3] -= y;
+  return true;
+}
 bool tjpg_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap) {
   // (x,y)~(x+w,y+h) 블록 단위 픽셀이 bitmap으로 들어옴
   // 여기서 특정 좌표의 RGB565 값을 추출 가능
   // Serial.printf("%d %d %d %d", x, y, w, h);
+  int sx_off_led = off_led_pos[0];
+  int sy_off_led = off_led_pos[1];
+  int ex_off_led = off_led_pos[0] + len_led_area;
+  int ey_off_led = off_led_pos[1] + len_led_area;
+  if (calc_cross_area(x, y, w, h, off_led_pos[0], off_led_pos[1], len_led_area, len_led_area)) {
+    for (int cur_x = ret_array[0]; cur_x <= ret_array[2]; cur_x++) {
+      for (int cur_y = ret_array[1]; cur_y <= ret_array[3]; cur_y++) {
+        uint16_t color = bitmap[cur_x * w + cur_y];
+        uint8_t r = ((color >> 11) & 0x1F) << 3;
+        uint8_t g = ((color >> 5) & 0x3F) << 2;
+        uint8_t b = (color & 0x1F) << 3;
+        off_led_power += r;
+        off_led_power += g;
+        off_led_power += b;
+      }
+    }
+  }
   for (int k = 0; k < 4; k++) {
     for (int idx = 0; idx < 8; idx++) {
       if (k == 0 && idx != 1 && idx != 2) {
@@ -17,16 +67,18 @@ bool tjpg_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t *bitmap)
       }
       int px = seg_pixel_pos[k][idx][0];
       int py = seg_pixel_pos[k][idx][1];
-      int local_x = px - x;
-      int local_y = py - y;
-      if (0 <= local_x && local_x < w && 0 <= local_y && local_y < h) {
-        uint16_t color = bitmap[local_y * w + local_x];
-        uint8_t r = ((color >> 11) & 0x1F) << 3;
-        uint8_t g = ((color >> 5) & 0x3F) << 2;
-        uint8_t b = (color & 0x1F) << 3;
-        segment_led_state[k][idx] = abs(r - TARGET_R) <= 40 && abs(g - TARGET_G) <= 40 && abs(b - TARGET_B) <= 40;
-        Serial.printf("%d %d %d\n", abs(r - TARGET_R), abs(g - TARGET_G), abs(b - TARGET_B));
-        Serial.printf("%d %d %d\n\n", r, g, b);
+      if (calc_cross_area(x, y, w, h, seg_pixel_pos[k][idx][0], seg_pixel_pos[k][idx][1], len_led_area, len_led_area)) {
+        for (int cur_x = ret_array[0]; cur_x <= ret_array[2]; cur_x++) {
+          for (int cur_y = ret_array[1]; cur_y <= ret_array[3]; cur_y++) {
+            uint16_t color = bitmap[cur_x * w + cur_y];
+            uint8_t r = ((color >> 11) & 0x1F) << 3;
+            uint8_t g = ((color >> 5) & 0x3F) << 2;
+            uint8_t b = (color & 0x1F) << 3;
+            segment_led_power[k][idx] += r;
+            segment_led_power[k][idx] += g;
+            segment_led_power[k][idx] += b;
+          }
+        }
       }
     }
   }
@@ -71,6 +123,14 @@ void setup() {
 
   memset(segment_led_state, 0, sizeof(segment_led_state));
   TJpgDec.drawJpg(0, 0, data, fileSize);
+  Serial.println(off_led_power);
+  for (int k = 0; k < 4; k++) {
+    for (int idx = 0; idx < 8; idx++) {
+      Serial.print(segment_led_power[k][idx]);
+      Serial.print(" ");
+    }
+    Serial.print("\n");
+  }
   print7segment(segment_led_state[0]);
   print7segment(segment_led_state[1]);
   print7segment(segment_led_state[2]);
