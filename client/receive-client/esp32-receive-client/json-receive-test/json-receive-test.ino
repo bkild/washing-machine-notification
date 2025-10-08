9d887bebd4b97041409f1eea5e7ba7a53ce11907
iyi7028 <iyi7028@gmail.com>
2025-10-08 17:20:20 +0900

    함수위치이동

--- client/receive-client/esp32-receive-client/json-receive-test/json-receive-test.ino
+++ client/receive-client/esp32-receive-client/json-receive-test/json-receive-test.ino
@@ -8,35 +8,43 @@ const char *host = "192.168.35.189";
 
 WiFiClient client;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   WiFi.begin(ssid, password);
-
+  WiFi.setSleep(false);
+  
   Serial.print("WiFi 연결중...");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   Serial.println("\nWiFi 연결 완료");
 
-  if (!client.connect(host, port)) {
+  if (!client.connect(host, port))
+  {
     Serial.println("서버 연결 실패");
     return;
   }
   Serial.println("서버 연결 성공");
 }
 
-void loop() {
+void loop()
+{
   // 서버 → ESP32 데이터 수신 처리
-  if (client.available() >= 4) {
+  if (client.available() >= 4)
+  {
     uint32_t lenNet;
-    client.readBytes((uint8_t*)&lenNet, 4);
+    client.readBytes((uint8_t *)&lenNet, 4);
     uint32_t length = ntohl(lenNet);
 
     String json;
     json.reserve(length);
-    while (json.length() < length) {
-      if (client.available()) {
+    while (json.length() < length)
+    {
+      if (client.available())
+      {
         json += (char)client.read();
       }
     }
@@ -47,7 +55,7 @@ void loop() {
     // 필요시 서버에 응답도 가능
     String reply = "{\"status\":\"received\"}";
     uint32_t replyLenNet = htonl(reply.length());
-    client.write((uint8_t*)&replyLenNet, 4);
-    client.write((const uint8_t*)reply.c_str(), reply.length());
+    client.write((uint8_t *)&replyLenNet, 4);
+    client.write((const uint8_t *)reply.c_str(), reply.length());
   }
 }
--- client/receive-client/esp32-receive-client/seven-segment-print-test/seven-segment-print-test.ino
+++ client/receive-client/esp32-receive-client/seven-segment-print-test/seven-segment-print-test.ino
@@ -1,23 +1,14 @@
 #include "seven_segment_led.h";
-int digit[] = { 18, 19, 22, 23 };
-int segment[] = { 12, 13, 14, 25, 26, 27, 32, 33 };
 void setup() {
   Serial.begin(115200);
-  seven_segment_init();
-  for (int i = 0; i < 8; i++) {
-    pinMode(segment[i], OUTPUT);
-  }
-
-  for (int i = 0; i < 4; i++) {
-    pinMode(digit[i], OUTPUT);
-  }
+  seven_segment_init(1);
 }
 
 int min_val = 60;
 int tmpCnt = 0;
 void loop() {
   if (min_val == 0) {
-    show_end();
+    show_err();
   } else {
     show_min(min_val);
     tmpCnt++;
@@ -32,37 +23,4 @@ void loop() {
 
 
   // cnt++;
-}
-void show_min(int min_val) {
-  for (int i = 3; i >= 0; i--) {
-    char c = min_val % 10 + '0';
-    show_char(i, c);
-    delay(5);
-    min_val /= 10;
-  }
-}
-void show_end() {
-  show_char(1, 'E');
-  delay(5);
-  show_char(2, 'N');
-  delay(5);
-  show_char(3, 'D');
-  delay(5);
-}
-void show_char(int pos, char c) {
-  for (int i = 0; i < 4; i++) {
-    if (i == pos) {
-      digitalWrite(digit[i], LOW);
-    } else {
-      digitalWrite(digit[i], HIGH);
-    }
-  }
-  for (int i = 0; i < 8; i++) {
-    byte segment_data = (char_to_seven_segment_idx[c] & (0x01 << i)) >> i;
-    if (segment_data == 1) {
-      digitalWrite(segment[7 - i], HIGH);
-    } else {
-      digitalWrite(segment[7 - i], LOW);
-    }
-  }
 }
