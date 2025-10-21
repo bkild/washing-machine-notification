608e6a8b05737f8d2dbf9f7af4e94da642789787
iyi7028 <iyi7028@gmail.com>
2025-10-21 22:33:43 +0900

    .env로 변수 관리

--- server/index.mjs
+++ server/index.mjs
@@ -1,8 +1,10 @@
 import net from "net";
 import { Buffer } from "buffer";
-const ESP32_TCP_SOCKET_PORT = 10032;
-const TCP_SOCKET_PORT = 10000;
-const WEB_SOCKET_PORT = 11000;
+import dotenv from "dotenv"
+dotenv.config()
+const ESP32_TCP_SOCKET_PORT = process.env.ESP32_TCP_SOCKET_PORT;
+const TCP_SOCKET_PORT = process.env.TCP_SOCKET_PORT;
+const WEB_SOCKET_PORT = process.env.WEB_SOCKET_PORT;
 
 let esp32Socket = null;
 let tcpSocketClients = [];
